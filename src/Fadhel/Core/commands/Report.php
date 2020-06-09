b78d13e12db3867ba712a3e2d77506a13afd1f55
Dim <62505585+DimBis@users.noreply.github.com>
2020-06-09 08:46:17 +0300

    Update Report.php

--- src/Fadhel/Core/commands/Report.php
+++ src/Fadhel/Core/commands/Report.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright 2019 Fadhel
+ * Copyright 2020 Fadhel
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
